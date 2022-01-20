6a1fc09f647b4a02af9577d4ddbd805e01760343
Jess Graves (she/her) <93943270+jessgr@users.noreply.github.com>
2022-01-20 13:48:32 -0500

    JG updates
    removing loop for increased speed

--- run_calcPCClocks.R
+++ run_calcPCClocks.R
@@ -46,16 +46,18 @@ calcPCClocks <- function(path_to_PCClocks_directory, datMeth, datPheno){
   #If needed: Fill in missing CpGs needed for calculation of PCs; use mean values from GSE40279 (Hannum 2013; blood)- note that for other tissues you might prefer to use a different one
   datMeth <- as.data.frame(datMeth)
   missingCpGs <- c(CpGs[!(CpGs %in% colnames(datMeth))])
-  datMeth[,missingCpGs] <- NA
-  for(i in 1:length(missingCpGs)){
-    datMeth[,missingCpGs[i]] <- imputeMissingCpGs[missingCpGs[i]]
-  }
+  datMeth[, missingCpGs] <- NA
+  # JG Edits to code 
+  # no more for loop, just merge in dataframe
+  impute.df <- data.frame(t(imputeMissingCpGs[missingCpGs]))
+  datMeth[, missingCpGs] <- impute.df
 
   #fill in CpGs that are NA for all values
-  missingCpGs <- CpGs[apply(datMeth[,CpGs], 2, function(x)all(is.na(x)))]
-  for(i in 1:length(missingCpGs)){
-    datMeth[,missingCpGs[i]] <- imputeMissingCpGs[missingCpGs[i]]
-  }
+  #fill in CpGs that are NA for all values
+  all_missingCpGs <- CpGs[apply(datMeth[,CpGs], 2, function(x)all(is.na(x)))]
+  # Hopefully won't throw an error anymore in event that there are no missing
+  datMeth[, all_missingCpGs] <- impute.df[,all_missingCpGs]
+                                
   message("Any missing CpGs successfully filled in (see function for more details)")
   
   #Prepare methylation data for calculation of PC Clocks (subset to 78,464 CpGs and perform imputation if needed)
